997aad4b4b872f2346c6d56f575c4aa0e8376f9d
Henry <141414208+henryarauj0@users.noreply.github.com>
2024-11-07 08:43:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criarCartao(
+    "filosofia",
+    "Quem ganhou a bola de ouro nessa temporada?",
+    "Albert Einstein"
+)
+
+criarCartao(
+    "Historia",
+    "Quem descobriu o Brasil?",
+    "Lula"
+)
+
+criarCartao(
+    "filosofia",
+    "Qual foi o melhor time do mundo na ultima temporada?",
+    "real madrid"
+)
+
+criarCartao(
+    "filosofia",
+    "Quem foi o melhor goleiro da copa america?",
+    "dibu martinez"
+)
+
+criarCartao(
+    "filosofia",
+    "Quem foi o melhor jogador jovem do mundo?",
+    "yamal"
+)
+
+criarCartao(
+    "filosofia",
+    "Quem disse a famosa frase 'eu sabo'?",
+    "albert einstein"
+)
