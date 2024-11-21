6f10dba7cf34c02b445defd19746f83ef7898fe0
Henry <141414208+henryarauj0@users.noreply.github.com>
2024-11-21 07:40:11 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 
-function criarCartao(materia, pergunta, resposta){
+function criarCartao(materia, pergunta, resposta) {
     let caixa = document.getElementById('caixa')
-    let cartao= document.createElement('article')
+    let cartao = document.createElement('article')
 
     cartao.className = `cartao`
 
     cartao.innerHTML = `
     <div class="cartao__conteudo">
-                <h3>${materia}</h3>
-                <div class="cartao__conteudo__pergunta">
-                    <p>${pergunta}</p>
-                </div>
-                <div class="cartao__conteudo__resposta">
-                    <p>${resposta}</p>
-                </div>
-            </div>
-    `
+        <h3>${materia}</h3>
+        <div class="cartao__conteudo__pergunta">
+            <p>${pergunta}</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+            <p>${resposta}</p>
+        </div>
+    </div>`
 
     caixa.appendChild(cartao)
 
